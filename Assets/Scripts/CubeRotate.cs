9e86f198bb488b42c7d3fe53723a188fd8044aec
Kendzeyyy <kendynguyen20@hotmail.com>
2018-11-18 00:11:59 +0200

    Added animations

---
+++ Assets/Scripts/CubeRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotate : MonoBehaviour {
+
+	Transform cmpTransform;
+	Vector3 rotationVector = new Vector3(0f, 0.2f, 0f);
+
+	// Use this for initialization
+	void Start () {
+		cmpTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cmpTransform.Rotate(rotationVector);
+	}
+}
