34b9794b873d5f8fd27230fbad2a5e9acd1a29b3
Kendzeyyy <kendynguyen20@hotmail.com>
2018-11-18 20:34:51 +0200

    new textures and moving animations

--- Assets/Scripts/CubeRotate.cs
+++ Assets/Scripts/CubeRotate.cs
@@ -7,13 +7,25 @@ public class CubeRotate : MonoBehaviour {
 	Transform cmpTransform;
 	Vector3 rotationVector = new Vector3(0f, 0.2f, 0f);
 
+	// Hover
+	public float verticalSpeed;
+	public float amplitude;
+	public Vector3 tempPosition;
+
 	// Use this for initialization
 	void Start () {
 		cmpTransform = GetComponent<Transform>();
+
+		// Hover
+		//tempPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		cmpTransform.Rotate(rotationVector);
+
+		// Hover
+		//tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+		//transform.position = tempPosition;
 	}
 }
---
+++ Assets/Scripts/MovingCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+
+	Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start()
+	{
+		originalPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Moving the cubes slowly
+		transform.Translate(0f, 0f, -0.1f * Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "End")
+		{
+			transform.position = originalPosition;
+		}
+	}
+}
