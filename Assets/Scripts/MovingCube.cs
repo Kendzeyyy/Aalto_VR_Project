d22b845872024c5274adb9c1a06c65c77cef422a
Kendy Nguyen <kendynguyen20@hotmail.com>
2018-11-19 14:13:49 +0200

    unity changes

--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
-
-	Vector3 originalPosition;
+ 
+    public float targetTime = 5.0f;
+    Vector3 originalPosition;
 
 	// Use this for initialization
 	void Start()
 	{
 		originalPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-	}
+        
+    }
 
 	// Update is called once per frame
 	void Update()
 	{
-		// Moving the cubes slowly
-		transform.Translate(0f, 0f, -0.1f * Time.deltaTime);
+        targetTime -= Time.deltaTime;
 
+        if (targetTime <= 0.0f)
+        {
+            // Moving the cubes slowly towards camera
+            transform.Translate(0f, 0f, -0.1f * Time.deltaTime);
+            timerEnded();
+        }
 	}
+
+    void timerEnded()
+    {
+        // Reverse cubes movements
+        
+    }
 
 	void OnTriggerEnter(Collider other)
 	{
