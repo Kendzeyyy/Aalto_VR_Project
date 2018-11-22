03a207f6a3c44b0ae2b5dde9a355d912e442a2d9
Mikael M <mikael_96@live.com>
2018-11-22 14:14:11 +0200

    air floating

---
+++ Assets/Scripts/AirFloating.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirFloating : MonoBehaviour {
+
+    public float FloatStrenght;
+    public float RandomRotationStrenght;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+       void Update()
+    {
+       // transform.GetComponent<Rigidbody>().AddForce(Vector3.up * FloatStrenght);
+        transform.GetComponent<Rigidbody>().AddForce(0, -5, 0);
+       
+        //transform.Rotate(RandomRotationStrenght, RandomRotationStrenght, RandomRotationStrenght);
+    }
+}
