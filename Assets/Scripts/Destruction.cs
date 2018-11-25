4821886f6c0a3fddc702890fa523a1079f7ad4e9
Kendzeyyy <kendynguyen20@hotmail.com>
2018-11-25 21:32:55 +0200

    minor changes on teleport script

--- Assets/Scripts/Destruction.cs
+++ Assets/Scripts/Destruction.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Destruction : MonoBehaviour {
 
 	public GameObject remains;
-	public GameObject cube1;
 
 	// Use this for initialization
 	void Start(){
 
 		remains = (Resources.Load("Assets/Prefabs/RemainsApi") as GameObject).gameObject;
 		//remains = (GameObject)Resources.Load("Assets/Prefabs/RemainsApi", typeof(GameObject));
-		cube1 = GameObject.FindGameObjectWithTag("cube1");
 	}
 
 	void OnTriggerEnter(Collider collider){
