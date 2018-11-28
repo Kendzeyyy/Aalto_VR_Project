91cebb0c5a05565b21b24cb7c91fc0d46914598b
Kendy Nguyen <kendynguyen20@hotmail.com>
2018-11-28 15:31:22 +0200

    Teleport player back in time

---
+++ Assets/Scripts/TimeTravel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravel : MonoBehaviour{
+
+    public Transform teleportTarget;
+    public GameObject player;
+    public GameObject year;
+    public string text;
+
+
+    // Use this for initialization
+    void Start(){
+        player = GameObject.FindWithTag("Player");
+        GetComponent<TextMesh>().text = "test";
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player"){
+            player.transform.position = teleportTarget.transform.position;
+        }
+    }
+}
