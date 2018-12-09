98632e86e9feb0aba225f9c5d0f1654eb17d75bb
Kendzeyyy <kendynguyen20@hotmail.com>
2018-12-09 23:07:50 +0200

    changes on sound

--- Assets/Scripts/Destruction.cs
+++ Assets/Scripts/Destruction.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class Destruction : MonoBehaviour {
 
 	public GameObject remains;
-
+	public AudioSource breakingbox;
 
 	// Use this for initialization
 	void Start(){
 
-		remains = (Resources.Load("Assets/Prefabs/RemainsApi") as GameObject).gameObject;
 		//remains = (GameObject)Resources.Load("Assets/Prefabs/RemainsApi", typeof(GameObject));
+		remains = (Resources.Load("Assets/Prefabs/RemainsBox") as GameObject).gameObject;
+
+		//breakingbox = (Resources.Load("Assets/Sound/BreakingSound" as AudioSource);
+		breakingbox = GetComponent<AudioSource>();
 	}
 
 	void OnTriggerEnter(Collider collider){
-		if (collider.gameObject.tag == "2019"){
-
+		if (collider.gameObject.tag == "2019")
+		{
             Vector3 temp = gameObject.transform.position;
 			Destroy(gameObject);
 			Instantiate(remains,temp,Quaternion.identity);
-
+			breakingbox.Play();
 		}
     }
 }
