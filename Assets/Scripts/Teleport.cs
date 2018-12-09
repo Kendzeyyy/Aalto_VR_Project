31d8284199651d8811c36a32beca9900f1311e85
Kendzeyyy <kendynguyen20@hotmail.com>
2018-12-09 20:07:39 +0200

    new changes on teleport

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Teleport : MonoBehaviour {
 
 	public Transform teleportTarget;
+	public Transform teleportTarget2;
 	public GameObject theCube1;
 	public GameObject theCube2;
 	public GameObject theCube3;
@@ -32,12 +33,139 @@ public class Teleport : MonoBehaviour {
 		theCube10 = GameObject.FindWithTag("cube10");
 		theCube11 = GameObject.FindWithTag("cube11");
 		theCube12 = GameObject.FindWithTag("cube12");
+	
 	}
 
-	void OnTriggerEnter(Collider collider)
-	{
-		if (collider.gameObject.tag == "cube1")
+	void OnTriggerEnter(Collider collider){
 
+		//--- Straps ---
+
+		if (collider.gameObject.name == "strap1")
+		{
+			GameObject strap1;
+			strap1 = GameObject.Find("strap1");
+			strap1.transform.position = teleportTarget2.transform.position;
+		}
+
+		else if (collider.gameObject.name == "strap2")
+		{
+			GameObject strap2;
+			strap2 = GameObject.Find("strap2");
+			strap2.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap3")
+		{
+			GameObject strap3;
+			strap3 = GameObject.Find("strap3");
+			strap3.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap4")
+		{
+			GameObject strap4;
+			strap4 = GameObject.Find("strap4");
+			strap4.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap5")
+		{
+			GameObject strap5;
+			strap5 = GameObject.Find("strap5");
+			strap5.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap6")
+		{
+			GameObject strap6;
+			strap6 = GameObject.Find("strap6");
+			strap6.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap7")
+		{
+			GameObject strap7;
+			strap7 = GameObject.Find("strap7");
+			strap7.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap8")
+		{
+			GameObject strap8;
+			strap8 = GameObject.Find("strap8");
+			strap8.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap9")
+		{
+			GameObject strap9;
+			strap9 = GameObject.Find("strap9");
+			strap9.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap10")
+		{
+			GameObject strap10;
+			strap10 = GameObject.Find("strap10");
+			strap10.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap11")
+		{
+			GameObject strap11;
+			strap11 = GameObject.Find("strap11");
+			strap11.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap12")
+		{
+			GameObject strap12;
+			strap12 = GameObject.Find("strap12");
+			strap12.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap13")
+		{
+			GameObject strap13;
+			strap13 = GameObject.Find("strap13");
+			strap13.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap14")
+		{
+			GameObject strap14;
+			strap14 = GameObject.Find("strap14");
+			strap14.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap15")
+		{
+			GameObject strap15;
+			strap15 = GameObject.Find("strap15");
+			strap15.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap16")
+		{
+			GameObject strap16;
+			strap16 = GameObject.Find("strap16");
+			strap16.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap17")
+		{
+			GameObject strap17;
+			strap17 = GameObject.Find("strap17");
+			strap17.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap18")
+		{
+			GameObject strap18;
+			strap18 = GameObject.Find("strap18");
+			strap18.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap19")
+		{
+			GameObject strap19;
+			strap19 = GameObject.Find("strap19");
+			strap19.transform.position = teleportTarget2.transform.position;
+		}
+		else if (collider.gameObject.name == "strap20")
+		{
+			GameObject strap20;
+			strap20 = GameObject.Find("strap20");
+			strap20.transform.position = teleportTarget2.transform.position;
+		}
+		
+
+		//--- cubes ---
+
+		if (collider.gameObject.tag == "cube1")
 		{
 			theCube1.transform.position = teleportTarget.transform.position;
 
