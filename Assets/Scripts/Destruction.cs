6eb0a1fe17001b45a85a5c31efd04ed0c61b66d1
Kendzeyyy <kendynguyen20@hotmail.com>
2018-12-11 22:26:14 +0200

    reset script function added

--- Assets/Scripts/Destruction.cs
+++ Assets/Scripts/Destruction.cs
@@ -10,27 +10,25 @@ public class Destruction : MonoBehaviour {
 
     // Use this for initialization
     void Start(){
+
         audioSource = GetComponent<AudioSource>();
-        collisionSound = GetComponent<AudioClip>();
 
         //remains = (GameObject)Resources.Load("Assets/Prefabs/RemainsApi", typeof(GameObject));
         remains = (Resources.Load("Assets/Prefabs/RemainsBox") as GameObject).gameObject;
         Debug.Log("Load remainbox api");
-        //breakingbox = (Resources.Load("Assets/Sound/BreakingSound") as AudioSource);
-
-        
+ 
     }
 
 	void OnTriggerEnter(Collider collider){
 		if (collider.gameObject.tag == "2019")
 		{
-            Vector3 temp = gameObject.transform.position;
+			Vector3 temp = gameObject.transform.position;
 			Destroy(gameObject);
 			Instantiate(remains,temp,Quaternion.identity);
-            audioSource = GetComponent<AudioSource>();
-            audioSource.clip = collisionSound;
-            audioSource.Play();
-            Debug.Log("toimiii");
+			//audioSource.Play();
+			GetComponent<AudioSource>().Play();
+			audioSource.PlayOneShot(collisionSound, 0.05f);
+			Debug.Log("Breaking sound played");
         }
     }
 }
--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -5,20 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Reset : MonoBehaviour {
 
-    private AssetBundle myLoadedAssetBundle;
-    private string[] scenePaths;
-
-    // Use this for initialization
-    void Start () {
-
-    }
-
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Reset")
         {
-            SceneManager.LoadScene("ObjectGrab", LoadSceneMode.Single);
-            Debug.Log("Reset scene");
+			Debug.Log("Reset scene");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
