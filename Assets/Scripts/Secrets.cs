f94557c12d2850831315b39ab28c805405d24475
Local user account by Powershell <medialabuser@ETY-ALIEN-002.edu.wks.metropolia.fi>
2018-12-13 15:17:32 +0200

    final day changes

--- Assets/Scripts/Secrets.cs
+++ Assets/Scripts/Secrets.cs
@@ -7,7 +7,12 @@ namespace Assets.Scripts
 {
     public static class Secrets
     {
+
         public const string myApiKey = "11a1bd06c2e866f8b431684d632e0f1a";
         public const string userid = "144032320@N08";
+
+        //Customers account.
+        //public const string userid = "158003454@N03";
+        //public const string myApiKey = "2c0d9b5393e4533f69ad98f5d3cd6d4b";
     }
 }
